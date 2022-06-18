f229ac9d9fb77d7b693a993e8963ddae2b0ead86
Ollin Magno <10153618-ollinmagno@users.noreply.gitlab.com>
2022-06-18 16:36:47 -0300

    first commit

---
+++ src/main/java/br/com/eventeasy/EventeasyApplication.java
@@ -0,0 +1,13 @@
+package br.com.eventeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventeasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eventeasy/EventeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eventeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
